a07f6411fa853a66b63fc4736c921a42418f2caf
Carolina Sánchez <femimagia@gmail.com>
2021-07-25 19:11:05 -0300

    HTML:agrego ids para modo oscuro, cambié labels por h3 y value de fondo, CSS: agregué propiedades para clase light-mode, cambié colores de input range, JS: rearmé la función de modo claro/modo oscuro de cero

--- main.js
+++ main.js
@@ -11,9 +11,10 @@ const darkModeButton = document.querySelector('#dark-mode-button')
 const h1 = document.querySelector('h1');
 
 // VARIABLES PANEL IMAGEN
+const imgPanel = document.querySelector('.img-panel'); 
 
 // Variables títulos
-const imgPanel = document.querySelector('.img-panel'); 
+
 const ImgTitle = document.querySelector('#img-title');
 const urlTitle = document.querySelector('#url-title');
 const filterTitle = document.querySelector('#filter-title');
@@ -22,8 +23,10 @@ const backgroundImgTitle = document.querySelector('#background-img-title');
 //Variables inputs
 
 
+
 // VARIABLES PANEL TEXTO
 
+const textPanel = document.querySelector('.text-panel');
 // Variables títulos
 
 const TextTitle = document.querySelector('#text-title');
@@ -34,34 +37,29 @@ const textColorTitle = document.querySelector('#text-color-title');
 const textBackgroundTitle = document.querySelector('#text-background-title');
 const textOutlineTitle = document.querySelector('#text-outline-title');
 const paddingTitle = document.querySelector('#padding-title');
-const lineSpacingTitle = document.querySelector('line-spacing-title');
+const lineSpacingTitle = document.querySelector('#line-spacing-title');
+
+// Variables input
+
+const topTextInput = document.getElementById("top-text-input");
+const topTextInMemebox = document.getElementById("top-text-in-memebox")
+const bottomTextInput = document.getElementById("bottom-text-input");
+const bottomTextInMemebox = document.getElementById("bottom-text-in-memebox");
+const memeBox = document.getElementById("meme-box")
+const hideTopText = document.getElementById("hide-top-text");
+const containerTopText = document.getElementById("container-top-text");
+const containerBottomText = document.getElementById("container-bottom-text")
+const hideBottomText = document.getElementById("hide-bottom-text");
+const fontSizeInput = document.getElementById("font-size-input");
 
 
 //-------------------------------FUNCIONALIDAD MODO OSCURO--------------------------------------//
 
-lightModeButton.style.display = 'block';
-darkModeButton.style.display = 'none';
+lightModeButton.style.display = 'none';
+darkModeButton.style.display = 'block';
 
 const switchMode = () => {
-    body.classList.toggle('dark-mode-workspace');
-    header.classList.toggle('dark-mode-header')
-    h1.classList.toggle('dark-mode-header');
-    textButton.classList.toggle('dark-mode-header');
-    imgButton.classList.toggle('dark-mode-header');
-    imgPanel.classList.toggle('dark-mode-aside');
-    ImgTitle.classList.toggle('dark-mode-aside');
-    urlTitle.classList.toggle('dark-mode-aside')
-    filterTitle.classList.toggle('dark-mode-aside')
-    backgroundImgTitle.classList.toggle('dark-mode-aside')
-    TextTitle.classList.toggle('dark-mode-aside')
-    topTextTitle.classList.toggle('dark-mode-aside')
-    bottomTextTitle.classList.toggle('dark-mode-aside')
-    fontTitle.classList.toggle('dark-mode-aside')
-    textColorTitle.classList.toggle('dark-mode-aside')
-    textBackgroundTitle.classList.toggle('dark-mode-aside')
-    textOutlineTitle.classList.toggle('dark-mode-aside')
-    paddingTitle.classList.toggle('dark-mode-aside')
-    lineSpacingTitle.classList.toggle('dark-mode-aside')
+    body.classList.toggle('light-mode')
 
     if (darkModeButton.style.display === 'none') {
         lightModeButton.style.display = 'none';
@@ -69,7 +67,6 @@ const switchMode = () => {
     } else {
         lightModeButton.style.display = 'block';
         darkModeButton.style.display = 'none';
-        
     }
 }
 
@@ -77,13 +74,7 @@ lightModeButton.onclick = switchMode;
 darkModeButton.onclick = switchMode;
 
 
-
 //-------- Funcionalidad Menú Texto---------//
-
-const topTextInput = document.getElementById("top-text-input");
-const topTextInMemebox = document.getElementById("top-text-in-memebox")
-const bottomTextInput = document.getElementById("bottom-text-input");
-const bottomTextInMemebox = document.getElementById("bottom-text-in-memebox");
 
 topTextInput.oninput = () => { 
     topTextInMemebox.textContent = topTextInput.value;
